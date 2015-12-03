18d6e465bc309ba016d12bd8ba229b454d8147d3
Martin Ryan <martinrryan@gmail.com>
2015-12-03 12:23:48 -0600

    starting point for basic sinatra server

--- app.rb
+++ app.rb
@@ -1,2 +1,11 @@
 require "bundler"
 Bundler.require
+
+ActiveRecord::Base.establish_connection(
+  :database => "bcrypt",
+  :adapter => "postgresql"
+)
+
+get "/" do
+  return {:message => "hello world"}.to_json
+end
